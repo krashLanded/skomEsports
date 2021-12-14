b33c8d35980e46c7831a9013d8230bc1022b7474
krashLanded <ericquesada02@icloud.com>
2021-12-13 16:31:16 -0800

    fix socials showing on menu click

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 
 const toggleButton = document.querySelector('.toggle-button')
 const navBarLinks = document.querySelector('.header-links')
+const socials = document.querySelector('.socials')
 
 toggleButton.addEventListener('click', () => {
     navBarLinks.classList.toggle('active');
+    socials.classList.toggle('active');
 });
 
